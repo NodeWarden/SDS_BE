7d83f3c3f2e00a8aa8950044357be870916435e9
NodeWarden <federicoserra.business@proton.me>
2025-07-02 16:40:03 +0200

    updated script: cycle breaks at header #4.1, collecting tables as chunk since we only need H codes and not the whole content, nor EUH codes

--- doc_converter.py
+++ doc_converter.py
@@ -1,34 +1,22 @@
-import logging
-import pandas as pd
 from pathlib import Path
-from docling.document_converter import DocumentConverter
-from docling.chunking import HybridChunker
-
+from langchain_docling import DoclingLoader
 
 FOLDER_PATH = Path("./data")
 file_list = list(FOLDER_PATH.glob("*"))
 out_dir = Path("./data/processed")
 out_dir.mkdir(parents=True, exist_ok=True)
 
-converter = DocumentConverter()
-chunker = HybridChunker()
+loader = DoclingLoader(file_path = file_list)
 
 for file_path in file_list:
     if file_path.is_file():
         loader = DoclingLoader(file_path = str(file_path))
         doc_iter = loader.lazy_load()
         for d in doc_iter:
-            if d.page_content.startswith("3.2"):
-                # conv_res = converter.convert(d.page_content)
-                for table_ix, table in enumerate(d.tables):
-                    table_df: pd.DataFrame = table.export_to_dataframe()
-                    print(f" ## Table {table_ix}")
-                    print(table_df.to_markdown())
-                    with open(out_dir / f"{file_path.stem}_chunk.txt", "a", encoding="utf-8") as f:
-                        f.write(table_df.to_markdown())
-                        print(f"Saved table to {out_dir / f'{file_path.stem}_chunk.txt_'}")
+            if d.page_content.startswith("4.1"):
                 break
             print(f"Document:  {d.metadata['source']}: {d.page_content=}")
             with open(out_dir / f"{file_path.stem}_chunk.txt", "a", encoding="utf-8") as f:
                 f.write(d.page_content)
+                f.write("\n")
                 print(f"Saved chunk to {out_dir / f'{file_path.stem}_chunk.txt'}")
