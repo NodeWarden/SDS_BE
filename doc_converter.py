2a24b43bfac926d41be2722a91dc12328c3dabb8
reecoo <federicoserra.business@proton.me>
2025-07-02 11:57:33 +0200

    fixed cycle break

--- doc_converter.py
+++ doc_converter.py
@@ -1,16 +1,15 @@
-
 from pathlib import Path
 from langchain_docling import DoclingLoader
 
-
-FOLDER_PATH = Path("./guidelines")
-file_list = list(FOLDER_PATH.glob("*")) 
+FOLDER_PATH = Path("./data")
+file_list = list(FOLDER_PATH.glob("*"))
 loader = DoclingLoader(file_path = file_list)
-
 
 for file_path in file_list:
     if file_path.is_file():
         loader = DoclingLoader(file_path = str(file_path))
         doc_iter = loader.lazy_load()
         for d in doc_iter:
-            print(f"Document: {d.metadata['source']}: {d.page_content=}")
+            if d.page_content.startswith("4.1"):
+                break
+            print(f"Document:  {d.metadata['source']}: {d.page_content=}")
